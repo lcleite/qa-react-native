e6f80d68a091eac0391f91144199f0974ae0b38d
Leandro Leite <leandro@mini-de-leandro.home>
2017-11-15 15:39:27 -0200

    Initial commit

---
+++ __tests__/App.js
@@ -0,0 +1,16 @@
+import 'react-native';
+import React from 'react';
+import App from '../App';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <App />
+  );
+});
+
+it('should return 2', () => {
+  expect(1 + 1).toBe(2);
+});
